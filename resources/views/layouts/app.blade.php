607a7aab27e66d8b623ae121ed228dff99fc088c
wangyucai <1228835707@qq.com>
2017-12-12 11:11:37 +0800

    Add  Laravel Mix and basic page layout and user auth and captcha

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+    <title>@yield('title', 'LaraBBS') - mybbs</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
