bee3cd3ce1214c3d693c0759191bc558a2ba3f1c
wangyucai <1228835707@qq.com>
2017-12-12 15:53:11 +0800

    Seeding topics and users data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
-
+use Carbon\Carbon;
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -15,11 +15,14 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
     static $password;
-
+    $now = Carbon::now()->toDateTimeString();
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'introduction' => $faker->sentence(), //Faker 是一个假数据生成库，sentence() 是 faker 提供的 API ，随机生成『小段落』文本
+        'created_at' => $now,
+        'updated_at' => $now,
     ];
 });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // 获取 Faker 实例
+        $faker = app(Faker\Generator::class);
+
+        // 头像假数据
+        $avatars = [
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
+        ];
+
+        // 生成数据集合
+        // factory(User::class) 根据指定的 User 生成模型工厂构造器，对应加载 UserFactory.php 中的工厂设置。
+        // times(10) 指定生成模型的数量，此处我们只需要生成 10 个用户数据。
+        // make() 方法会将结果生成为 集合对象。
+        // each() 是 集合对象 提供的 方法，用来迭代集合中的内容并将其传递到回调函数中。
+        // use 是 PHP 中匿名函数提供的本地变量传递机制，匿名函数中必须通过 use 声明的引用，才能使用本地变量。
+        $users = factory(User::class)
+                        ->times(10)
+                        ->make()
+                        ->each(function ($user, $index)
+                            use ($faker, $avatars)
+        {
+            // 从头像数组中随机取出一个并赋值
+            $user->avatar = $faker->randomElement($avatars);
+        });
+
+        // 让隐藏字段可见，并将数据集合转换为数组
+        // makeVisible() 是 Eloquent 对象提供的方法，可以显示 User 模型 $hidden 属性里指定隐藏的字段，此操作确保入库时数据库不会报错。
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        // 插入到数据库中
+        User::insert($user_array);
+
+        // 单独处理第一个用户的数据
+        $user = User::find(1);
+        $user->name = 'wangyucai';
+        $user->email = '1228835707@qq.com';
+        $user->avatar = 'http://mybbs.gov.cn/uploads/images/avatars/201712/12/1_1513057537_4KaPVMmpbe.jpg';
+        $user->save();
+
+    }
+}
