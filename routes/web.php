7f4ef3e26cffaac12f901c1eff611f19ae238a26
wangyucai <1228835707@qq.com>
2017-12-12 14:56:34 +0800

    Generated topics

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,6 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 // Route::get('users/{user}/edit', 'UsersController@edit')->name('users.edit');
 // // 处理 edit 页面提交的更改
 // Route::patch('users/{user}', 'UsersController@update')->name('users.update');
+
+Route::resource('projects', 'ProjectsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
